05ad5c7d9627ae1c713af9a29aa74ef4c90610f6
Uğur <ugur.altnsy@gmail.com>
2026-03-07 21:03:50 +0300

    2.21v

--- src/data/www/meshchat/chat.js
+++ src/data/www/meshchat/chat.js
@@ -20,20 +20,20 @@ function init_dark_mode() {
     var saved = Cookies.get('meshchat_dark_mode');
     if (saved === '1') {
         $('body').addClass('dark-mode');
-        $('#dark-mode-icon').attr('src', 'assets/moon.svg');
+        $('#dark-mode-icon').attr('src', 'moon.svg');
     } else {
-        $('#dark-mode-icon').attr('src', 'assets/sun.svg');
+        $('#dark-mode-icon').attr('src', 'sun.svg');
     }
 
     $('#dark-mode-toggle').on('click', function () {
         var isDark = $('body').hasClass('dark-mode');
         if (isDark) {
             $('body').removeClass('dark-mode');
-            $('#dark-mode-icon').attr('src', 'assets/sun.svg');
+            $('#dark-mode-icon').attr('src', 'sun.svg');
             Cookies.set('meshchat_dark_mode', '0', { expires: 365 });
         } else {
             $('body').addClass('dark-mode');
-            $('#dark-mode-icon').attr('src', 'assets/moon.svg');
+            $('#dark-mode-icon').attr('src', 'moon.svg');
             Cookies.set('meshchat_dark_mode', '1', { expires: 365 });
         }
     });
