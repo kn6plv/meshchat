db5cce929945e5636fa61ac4cad592e69866e9a9
Uğur <ugur.altnsy@gmail.com>
2026-03-07 21:07:23 +0300

    2.21v

--- src/data/www/meshchat/shared.js
+++ src/data/www/meshchat/shared.js
@@ -1,12 +1,36 @@
 var config;
 
-$(function() {
-    $('#logout').on('click', function(e){
+$(function () {
+    init_dark_mode();
+    $('#logout').on('click', function (e) {
         e.preventDefault();
         Cookies.remove('meshchat_call_sign');
         window.location = '/meshchat';
     });
 });
+
+function init_dark_mode() {
+    var saved = Cookies.get('meshchat_dark_mode');
+    if (saved === '1') {
+        $('body').addClass('dark-mode');
+        $('#dark-mode-icon').attr('src', 'moon.svg');
+    } else {
+        $('#dark-mode-icon').attr('src', 'sun.svg');
+    }
+
+    $('#dark-mode-toggle').on('click', function () {
+        var isDark = $('body').hasClass('dark-mode');
+        if (isDark) {
+            $('body').removeClass('dark-mode');
+            $('#dark-mode-icon').attr('src', 'sun.svg');
+            Cookies.set('meshchat_dark_mode', '0', { expires: 365 });
+        } else {
+            $('body').addClass('dark-mode');
+            $('#dark-mode-icon').attr('src', 'moon.svg');
+            Cookies.set('meshchat_dark_mode', '1', { expires: 365 });
+        }
+    });
+}
 
 function node_name() {
     return config.node;
@@ -24,7 +48,7 @@ function format_date(date) {
     var day = date.getDate();
     var month = date.getMonth() + 1;
     var year = date.getFullYear();
-    
+
     var hours = date.getHours();
     var minutes = date.getMinutes();
     minutes = minutes < 10 ? '0' + minutes : minutes;
@@ -33,12 +57,11 @@ function format_date(date) {
     return day + '.' + month + '.' + year + '<br/>' + hours + ':' + minutes;
 }
 
-function make_id()
-{
+function make_id() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( var i=0; i < 5; i++ )
+    for (var i = 0; i < 5; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
